5b693657590fe70c847cb9574a820b89a316d00b
Caitlyn Tripician <ctripician06@gmail.com>
2024-11-13 17:24:52 -0500

    commit

--- whackamole.py
+++ whackamole.py
@@ -1,11 +1,11 @@
 import pygame
 
-
 def main():
     try:
         pygame.init()
         # You can draw the mole with this snippet:
         # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
+        #change
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
